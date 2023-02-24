56b1c46e679effd9e55cb0366ad4e3fb36693501
A.S.Rawat <adityasrwt5@gmail.com>
2023-02-24 15:46:40 +0530

    Grid Placement

--- Rogue Dungeon/Assets/Script/Player.cs
+++ Rogue Dungeon/Assets/Script/Player.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private void Start()
+    [SerializeField] float playerSpeed;
+
+    private void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
+        Vector2 dir = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -.5f;
+            //animator.SetInteger("Direction", 3);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = .5f;
+            //animator.SetInteger("Direction", 2);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y = .5f;
+            //animator.SetInteger("Direction", 1);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir.y = -.5f;
+            //animator.SetInteger("Direction", 0);
+        }
+
+        dir.Normalize();
+        //animator.SetBool("IsMoving", dir.magnitude > 0);
+
+        GetComponent<Rigidbody2D>().velocity = playerSpeed * dir;
     }
 
 }
