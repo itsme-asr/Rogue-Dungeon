aa01014f994c849eec0d5475834df2db0c9dac5b
A.S.Rawat <adityasrwt5@gmail.com>
2023-02-24 17:25:13 +0530

    Grid Base Movement

---
+++ Rogue Dungeon/Assets/Script/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Tilemap groundTileMap;
+    [SerializeField] Tilemap collisionTileMap;
+    private PlayerMovement controls;
+
+    private void Awake()
+    {
+        controls = new PlayerMovement();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    void Start()
+    {
+        controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (CanMove(direction))
+        {
+            transform.position += (Vector3)direction;
+        }
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = groundTileMap.WorldToCell(transform.position + (Vector3)direction);
+        if (!groundTileMap.HasTile(gridPosition) || collisionTileMap.HasTile(gridPosition))
+            return false;
+        return true;
+    }
+    void Update()
+    {
+
+    }
+}
