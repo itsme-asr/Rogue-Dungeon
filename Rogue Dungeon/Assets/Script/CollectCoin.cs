77d826dfbbe694b851aa964fc223f47f18360d09
A.S.Rawat <adityasrwt5@gmail.com>
2023-03-01 22:21:08 +0530

    Update

--- Rogue Dungeon/Assets/Script/CollectCoin.cs
+++ Rogue Dungeon/Assets/Script/CollectCoin.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CollectCoin : MonoBehaviour
 {
-
+    [SerializeField] GameObject pointsCollectionEffect;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Coin")
         {
-            Debug.Log("Collision");
+            GameObject effect = Instantiate(pointsCollectionEffect, transform.position, Quaternion.identity);
+            Destroy(effect, .75f);
+            //Debug.Log("Collision");
             //            GameObject.FindWithTag("Coin").transform.GetComponent<Score>().ScorePoints += 1;
             GameObject.Find("Score").GetComponent<Score>().ScorePoints += 1;
         }
