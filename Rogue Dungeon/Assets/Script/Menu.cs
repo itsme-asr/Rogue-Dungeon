e3c4edb0d6e84139ec38de5b277e34c7ee2f312b
A.S.Rawat <adityasrwt5@gmail.com>
2023-03-13 12:56:15 +0530

    Final

--- Rogue Dungeon/Assets/Script/Menu.cs
+++ Rogue Dungeon/Assets/Script/Menu.cs
@@ -18,11 +18,11 @@ public class Menu : MonoBehaviour
 
     public void nextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void instruction()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
